2a2b796df830389b717d64f634b210a7145e5732
yrondon757 <yrondon757@gmail.com>
2025-03-10 16:21:42 -0400

    1

--- proyecto-final/src/components/Footer.jsx
+++ proyecto-final/src/components/Footer.jsx
@@ -1,10 +1,54 @@
 function Footer() {
   const styles = {
-    footer: "bg-indigo-900 w-full h-[140px] flex justify-center items-center text-white",
+    footer:"bg-white w-full h-[500px] col justify-center text-white",
+    div: "w-full h-[90%] bg-gradient-to-b from-[#19899B]/40 to-[#0C0C96]/40 flex flex-col justify-end items-center",
+    div2: "w-full h-[10%] text-black text-2xl bg-[#19899B] flex justify-center items-center font-bold"
   };
   return (
     <footer className={styles.footer}>
-      <p>Copyright2025</p>
+      <div className={styles.div}>
+        <div className="w-[90%] h-[80%] text-black flex justify-between items-center border-t-4">
+          <div className="space-y-4 h-[250px] flex flex-col justify-center">
+            <p className="text-sm"><span className="font-bold text-3xl">Star</span><span className="text-[#19899B] font-bold text-3xl">Link</span><br />Internet de alta velocidad...</p>
+            <p className="text-sm"><span className="font-bold text-base">Direccion:</span><br/>Caracas 1030<br/>Distrito Capital</p>
+            <p className="text-sm"><span className="font-bold text-base">Detalle de Contactos:</span><br/>+58 424 1223 45 67<br/>+58 412 987 65 43</p>
+          </div>
+          <div className="space-y-4 h-[250px] flex flex-col justify-center">
+              <h3 className="font-bold text-[#19899B] text-base">Servicios</h3>
+              <li>Internet</li>
+              <li>Soporte IT</li>
+              <li>CCTV</li>
+              <li>Redes</li>
+              <li>Servidores</li>
+          </div>
+          <div className="space-y-4 h-[250px] flex flex-col justify-center">
+            <h3 className="font-bold text-[#19899B] text-base">Productos</h3>
+            <li>WIFI Routers</li>
+            <li>Impresoras Multifuncionales</li>
+            <li>Camaras, DVR, NVR</li>
+            <li>Routers, Switches</li>
+            <li>PCs, Laptops, Monitores</li>
+          </div>
+          <div className="space-y-4 h-[250px] flex flex-col justify-start">
+            <h3 className="font-bold text-[#19899B] text-base">Legal</h3>
+            <li>Contacto</li>
+            <li>Politica de Privacidad</li>
+            <li>Terminos & Condiciones</li>
+          </div>
+        </div>
+        <div className="w-[90%] h-[10%] flex justify-end items-center">
+          <div className="flex justify-center items-center gap-2 pb-4 text-[#19899B]">
+            <a href="https://www.facebook.com/"><img src="https://banesco-prod-2020.s3.amazonaws.com/wp-content/themes/banescocontigo/assets/images/social-media/facebook-circle-white.svg.gzip" alt="facebook" /></a>
+            <a href="https://www.instagram.com/"><img src="https://banesco-prod-2020.s3.amazonaws.com/wp-content/themes/banescocontigo/assets/images/social-media/instagram-circle-white.svg.gzip" alt="instagram" /></a>
+            <a href="https://www.linkedin.com/"><img src="https://banesco-prod-2020.s3.amazonaws.com/wp-content/themes/banescocontigo/assets/images/social-media/linkedin-circle-white.svg.gzip" alt="linkedin" /></a>
+            <a href="https://www.youtube.com/"><img src="https://banesco-prod-2020.s3.amazonaws.com/wp-content/themes/banescocontigo/assets/images/social-media/youtube-circle-white.svg.gzip" alt="youtube" /></a>
+            <a href="https://www.twitter.com/"><img src="https://banesco-prod-2020.s3.amazonaws.com/wp-content/themes/banescocontigo/assets/images/social-media/x-twitter-circle-white.svg.gzip" alt="twitter" /></a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.div2}>
+        <p>© 2025 StarLink</p>
+      </div>
     </footer>
   );
 }
--- proyecto-final/src/components/Navbar.jsx
+++ proyecto-final/src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 function Navbar() {
     const styles = {
-      nav: "bg-indigo-900 w-full h-[70px] flex justify-between items-center px-10",
-      p: "text-white text-3xl font-bold",
-      ul: "flex gap-4 text-white",
-      button: "bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded",
-      button2: "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded",
+      nav: "bg-[#F7F7F7] w-full h-[70px] flex justify-between items-center px-15 border-b-3 border-[#19899B]",
+      p: "text-black text-3xl font-bold",
+      ul: " font-bold text-lg flex gap-10 text-black",
+      button: "w-[120px] bg-white hover:bg-[#D4D1D1] text-[#19899B] font-bold py-2 px-4 border-2 border-[#19899B] rounded-lg cursor-pointer",
+      button2: "w-[120px] bg-[#19899B] hover:bg-[#104B55] text-white font-bold py-2 px-4 rounded-lg cursor-pointer",
     }
     return (
       <nav className={styles.nav}>
         <p className={styles.p}>LOGO</p>
         <ul className={styles.ul}>
-          <li>Inicio</li>
-          <li>Nosotros</li>
-          <li>Servicios</li>
-          <li>Contacto</li>
+          <li className="hover:text-[#19899B] cursor-pointer">Inicio</li>
+          <li className="hover:text-[#19899B] cursor-pointer">Nosotros</li>
+          <li className="hover:text-[#19899B] cursor-pointer">Servicios</li>
+          <li className="hover:text-[#19899B] cursor-pointer">Contacto</li>
         </ul>
         <div className="flex gap-7">
           <button className={styles.button}>Ingresar</button>
